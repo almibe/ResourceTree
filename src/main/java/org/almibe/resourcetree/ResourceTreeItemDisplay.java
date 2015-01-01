f6a52b632fb121b5fdc4286f74cf8522444b01d6
Alex Berry <berry3@indiana.edu>
2015-01-01 16:02:56 -0500

    switching image to imageview

--- src/main/java/org/almibe/resourcetree/ResourceTreeItemDisplay.java
+++ src/main/java/org/almibe/resourcetree/ResourceTreeItemDisplay.java
@@ -2,9 +2,9 @@ package org.almibe.resourcetree;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.StringProperty;
-import javafx.scene.image.Image;
+import javax.swing.text.html.ImageView;
 
 public interface ResourceTreeItemDisplay<T> {
     StringProperty getName(T t);
-    ObjectProperty<Image> getIcon(T t);
+    ObjectProperty<ImageView> getIcon(T t);
 }
