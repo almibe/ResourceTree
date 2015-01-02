81e4bf4d08a9baee7960cbf3a2ea9f02be9c5d5a
Alex Berry <berry3@indiana.edu>
2015-01-02 00:06:13 -0500

    clean up

--- src/main/java/org/almibe/resourcetree/ResourceTreeItemDisplay.java
+++ src/main/java/org/almibe/resourcetree/ResourceTreeItemDisplay.java
@@ -3,7 +3,6 @@ package org.almibe.resourcetree;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.Node;
-import javafx.scene.image.ImageView;
 
 public interface ResourceTreeItemDisplay<T> {
     StringProperty getName(T t);
