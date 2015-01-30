d65ddfec0f8a08a3779c1e28ed650cade062aa67
Alex Berry <berry3@indiana.edu>
2015-01-29 22:36:09 -0500

    adding empty class for JsonPersistence

---
+++ src/main/java/org/almibe/resourcetree/impl/JsonPersistence.java
@@ -0,0 +1,42 @@
+package org.almibe.resourcetree.impl;
+
+import org.almibe.resourcetree.ResourceTreeModeler;
+import org.almibe.resourcetree.ResourceTreePersistence;
+import org.almibe.resourcetree.TreeModel;
+
+public class JsonPersistence implements ResourceTreePersistence {
+    @Override
+    public void move(Object node, Object parent) {
+
+    }
+
+    @Override
+    public void add(Object node) {
+
+    }
+
+    @Override
+    public void add(Object node, Object parent) {
+
+    }
+
+    @Override
+    public void remove(Object node) {
+
+    }
+
+    @Override
+    public void update(Object node) {
+
+    }
+
+    @Override
+    public void load(TreeModel treeModel) {
+
+    }
+
+    @Override
+    public void load(TreeModel treeModel, ResourceTreeModeler resourceTreeModeler) {
+
+    }
+}
