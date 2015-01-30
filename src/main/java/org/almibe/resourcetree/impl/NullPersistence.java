537bfa45798617b204b62643ee44c26fbecbd72f
Alex Berry <berry3@indiana.edu>
2015-01-29 21:14:11 -0500

    implementing load in null persistence

--- src/main/java/org/almibe/resourcetree/impl/NullPersistence.java
+++ src/main/java/org/almibe/resourcetree/impl/NullPersistence.java
@@ -1,6 +1,7 @@
 package org.almibe.resourcetree.impl;
 
 import org.almibe.resourcetree.ResourceTreePersistence;
+import org.almibe.resourcetree.TreeModel;
 
 public class NullPersistence<T> implements ResourceTreePersistence<T> {
     @Override public void move(T node, T parent) {}
@@ -8,4 +9,5 @@ public class NullPersistence<T> implements ResourceTreePersistence<T> {
     @Override public void add(T node, T parent) {}
     @Override public void remove(T node) {}
     @Override public void update(T node) {}
+    @Override public void load(TreeModel<T> treeModel) {}
 }
