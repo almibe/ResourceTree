90c43ccd3c78ac352285bf358d7642866062b6b8
Alex Berry <berry3@indiana.edu>
2015-01-29 22:35:30 -0500

    adding first passing load spec tests

---
+++ src/test/groovy/org/almibe/resourcetree/LoadSpec.groovy
@@ -0,0 +1,27 @@
+import javafx.embed.swing.JFXPanel
+import org.almibe.resourcetree.demo.FolderResource
+import org.almibe.resourcetree.demo.Resource
+import org.almibe.resourcetree.impl.NullPersistence
+import org.almibe.resourcetree.impl.TreeViewResourceTree
+import spock.lang.Shared
+import spock.lang.Specification
+
+class LoadSpec extends Specification {
+    @Shared
+    JFXPanel fxPanel = new JFXPanel(); //this is a work around to init the JavaFX toolkit and is never used
+
+    @Shared
+    TreeViewResourceTree<Resource> treeViewResourceTree
+
+    def setupSpec() {
+        treeViewResourceTree = new TreeViewResourceTree<>(new FolderResource(""), false)
+        treeViewResourceTree.setTreePersistence(new NullPersistence<Resource>())
+    }
+
+    def 'load null resource model should throw illegal arguments exception'() {
+        when:
+        treeViewResourceTree.load(null)
+        then:
+        thrown(IllegalArgumentException)
+    }
+}
