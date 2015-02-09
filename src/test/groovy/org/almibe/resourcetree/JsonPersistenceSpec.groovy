fe7b421808ba678230ad24f712c78a76a98f00c5
Alex Berry <berry3@indiana.edu>
2015-02-09 17:53:51 -0500

    adding failing test

--- src/test/groovy/org/almibe/resourcetree/JsonPersistenceSpec.groovy
+++ src/test/groovy/org/almibe/resourcetree/JsonPersistenceSpec.groovy
@@ -1,5 +1,9 @@
 package org.almibe.resourcetree
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import javafx.embed.swing.JFXPanel
+import org.almibe.resourcetree.demo.FileResource
 import org.almibe.resourcetree.demo.Resource
 import org.almibe.resourcetree.impl.JsonPersistence
 import org.almibe.resourcetree.impl.TreeViewResourceTree
@@ -16,6 +20,9 @@ public class JsonPersistenceSpec extends Specification {
     @ClassRule
     TemporaryFolder temporaryFolder
 
+    @Shared
+    Gson gson = new Gson();
+
     TreeViewResourceTree<Resource> treeViewResourceTree
 
     File jsonFile
@@ -26,14 +33,17 @@ public class JsonPersistenceSpec extends Specification {
         treeViewResourceTree.setTreePersistence(new JsonPersistence<Resource>(jsonFile))
     }
 
-//    def 'adding a single node'() {
-//        given:
-//
-//        when:
-//
-//        then:
-//        treeViewResourceTree.rootItems.size() == 1
-//    }
+    def 'adding a single node'() {
+        when:
+        treeViewResourceTree.add(new FileResource("Test"))
+        then:
+        treeViewResourceTree.rootItems.size() == 1
+        Reader reader = new FileReader(jsonFile);
+        List<Resource> resources = gson.fromJson(reader, new TypeToken<List<Resource>>(){}.getType());
+        println(reader.text)
+        reader.close();
+        resources.size() == 1
+    }
 //
 //    def 'add two child nodes'() {
 //        given:
