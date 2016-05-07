78e88dae232dce84c01865dfb8ac03b301527cea
Alex Berry <berry3@indiana.edu>
2016-05-07 15:46:54 -0400

    removing commented out methods

--- src/main/java/org/almibe/resourcetree/api/ResourceTreePersistence.java
+++ src/main/java/org/almibe/resourcetree/api/ResourceTreePersistence.java
@@ -3,17 +3,6 @@ package org.almibe.resourcetree.api;
 import org.almibe.resourcetree.ResourceTree;
 
 public interface ResourceTreePersistence<T> {
-//    void move(T node, T parent);
-    //    void move(Collection<T> nodes, T parent);
-//    void add(T node);
-//    void add(T node, T parent);
-    //    void add(Collection<T> nodes);
-//    void add(Collection<T> nodes, T parent);
-//    void remove(T node);
-    //    void remove(Collection<T> nodes);
-//    void update(T node);
-    //    void update(Collection<T> nodes);
     void save(ResourceTree<T> resourceTree);
     void load(ResourceTree<T> resourceTree);
-//    void clear(ResourceTree<T> resourceTree);
 }
